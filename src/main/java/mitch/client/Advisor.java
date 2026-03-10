0b9fc13f2765e76ccf313566d840f48019c264fb
MitchL18 <mitchlevy30@gmail.com>
2026-03-10 19:44:40 -0400

    Added advisor class plugin.

---
+++ src/main/java/mitch/client/Advisor.java
@@ -0,0 +1,16 @@
+package mitch.client;
+
+import charlie.card.Card;
+import charlie.card.Hand;
+import charlie.plugin.IAdvisor;
+import charlie.util.Play;
+
+public class Advisor implements IAdvisor {
+
+    BasicStrategy bs = new BasicStrategy();
+
+    @Override
+    public Play advise(Hand hand, Card upCard) {
+        return bs.getPlay(hand, upCard);
+    }
+}
